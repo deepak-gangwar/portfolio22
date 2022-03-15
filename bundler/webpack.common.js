17a486dd5ae7e8c9222ff52dee333e990c52b26b
Deepak <deepakgangwar4265@gmail.com>
2022-03-15 09:15:08 +0530

    Updates

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -32,7 +32,7 @@ module.exports = {
 
     output:
     {
-        // filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, '../public')
     },
 
