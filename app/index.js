6850f1729ce6ffc02ddfd28cf9f27ebe15f376f2
Deepak <deepakgangwar4265@gmail.com>
2022-08-28 12:19:47 +0530

    Add cool console message for devs

--- app/index.js
+++ app/index.js
@@ -15,7 +15,8 @@ class App {
         }
 
         this.init()
-
+        this.styleConsoleForDevs()
+        
         // play animation only on larger viewports
         // this is not perfect solution. Make a resize function
         // because this fucks up when larger window is resized
@@ -47,6 +48,33 @@ class App {
 
     show() {
         new Show(this.elements)
+    }
+
+    styleConsoleForDevs() {
+        // 1. Pass CSS styles in an array
+        const style1 = [
+            'font-family: Helvetica',
+            'padding-top: 20px',
+            'padding-bottom: 20px',
+            'font-size: 1.2rem',
+        ].join(';')// 2. Concatenate the individual array item and concatenate them into a string separated by a semi-colon (;)
+    
+        const style2 = [
+            'color: red',
+            'padding-top: 20px',
+            'padding-bottom: 20px',
+            'font-size: 1.2rem',
+        ].join(';')
+    
+        const styleLink = [
+            'font-size: .7rem',
+            'padding-bottom: 20px',
+            // 'font-family: Helvetica',
+        ].join(';')
+    
+        // 3. Pass the styles variable
+        console.log('%cMade with %c♥%c by Deepak Gangwar%c\n✌ https://deepakgangwar.me', style1, style2, style1, styleLink)
+        // console.log('http://deepakgangwar.me');
     }
 }
 
