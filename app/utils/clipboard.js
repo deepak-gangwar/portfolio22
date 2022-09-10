cae05fdf55221b265f5c973b0f35015cd5b32a91
Deepak <deepakgangwar4265@gmail.com>
2022-09-10 15:19:57 +0530

    Improve CTA button by adding hover state

--- app/utils/clipboard.js
+++ app/utils/clipboard.js
@@ -1,14 +1,22 @@
 export default class Clipboard {
     constructor() {
+        this.bind()
+
         this.email = "deepakgangwar4265@gmail.com"
         this.headerItems = document.querySelectorAll('.header__item')
         this.contactBtn = this.headerItems[2]
 
-        this.text = document.querySelectorAll('.contact')
-        this.t1 = this.text[0]
-        this.t2 = this.text[1]
+        this.texts = document.querySelectorAll('.contact')
+
+        // this.t1 = this.text[0]
+        // this.t2 = this.text[1]
+        // this.t3 = this.text[2]
         
-        this.handleClick()
+        this.init()
+    }
+
+    bind() {
+        ['on', 'off', 'handleClick'].forEach(fn => this[fn] = this[fn].bind(this))
     }
 
     copyToClipboardAsync(str) {
@@ -18,16 +26,46 @@ export default class Clipboard {
     };
 
     handleClick() {
-        this.contactBtn.addEventListener('click', () => {
-            this.copyToClipboardAsync(this.email)
-
-            this.t1.style.transform = 'translateY(-100%)'
-            this.t2.style.transform = 'translateY(-100%)'
+        this.copyToClipboardAsync(this.email)
+        this.texts.forEach(text => {
+            text.style.transform = `translateY(-200%)`
         })
+        // this.t1.style.transform = 'translateY(-200%)'
+        // this.t2.style.transform = 'translateY(-200%)'
+        // this.t3.style.transform = 'translateY(-200%)'
+    }
 
-        this.contactBtn.addEventListener('mouseleave', () => {
-            this.t1.style.transform = 'translateY(0)'
-            this.t2.style.transform = 'translateY(0)'
+    on() {
+        this.texts.forEach(text => {
+            text.style.transform = `translateY(-100%)`
         })
+        // this.t1.style.transform = 'translateY(-100%)'
+        // this.t2.style.transform = 'translateY(-100%)'
+        // this.t3.style.transform = 'translateY(-100%)'
+    }
+
+    off() {
+        this.texts.forEach(text => {
+            text.style.transform = `translateY(0)`
+        })
+        // this.t1.style.transform = 'translateY(0)'
+        // this.t2.style.transform = 'translateY(0)'
+        // this.t3.style.transform = 'translateY(0)'
+    }
+
+    addEventListeners() {
+        this.contactBtn.addEventListener('mouseenter', this.on)
+        this.contactBtn.addEventListener('mouseleave', this.off)
+        this.contactBtn.addEventListener('click', this.handleClick)
+    }
+    
+    removeEventListeners() {
+        this.contactBtn.removeEventListener('mouseenter', this.on)
+        this.contactBtn.removeEventListener('mouseleave', this.off)
+        this.contactBtn.removeEventListener('click', this.handleClick)
+    }
+
+    init() {
+        this.addEventListeners()
     }
 }
